bedd6b5ff0c5237d42ca3e61dbaab802c8f0059f
saranatour1 <saranatourcontact@gmail.com>
2024-02-11 09:53:19 +0200

    TAS-49/Chore:base code updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import { MapContainer, ImageOverlay, Rectangle } from "react-leaflet";
 import "./App.css";
 import "leaflet/dist/leaflet.css";
-import { Suspense, useCallback, useEffect, useRef, useState } from "react";
-import { CRS, LatLngBoundsExpression, Map } from "leaflet";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { CRS, LatLngBoundsExpression } from "leaflet";
 import "./components/Form.css";
 
-import Point from "./components/Point";
-import PointToObserve from "./components/PointToObserve";
 function App() {
   const containerRef = useRef<HTMLDivElement | null>(null);
 
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
-  const [image, setImage] = useState(
-    "https://unsplash.com/photos/-hI5dX2ObAs/download"
-  );
+  const [image, setImage] = useState("https://unsplash.com/photos/-hI5dX2ObAs/download");
 
   useEffect(() => {
     const imgaeToBeLoaded = new Image();
@@ -25,57 +21,18 @@ function App() {
     };
   }, [image]);
 
-  const someText: string[] = [
-    "start",
-    "Hiiiii 1",
-    "Hiiiii 2",
-    "Hiiiii 3",
-    "Hiiii 4",
-    "finish",
-  ];
-
-  const innerBounds: LatLngBoundsExpression = [
-    [height - 200, width - 200], // y x
-    [height - 900, width - 900], // y x
-  ];
-
-  const innerBounds1: LatLngBoundsExpression = [
-    [height - 1200, width - 200], // y x
-    [height - 900, width - 1900], // y x
-  ];
-
-  const innerBounds2: LatLngBoundsExpression = [
-    [height - 1500, width - 1800], // y x
-    [height - 1800, width - 1900], // y x
-  ];
-
-  const outerBounds: LatLngBoundsExpression = [
-    [height - 1600, width - 1600],
-    [-height + 1000, -width + 1000],
-  ];
-
   const allBounds: LatLngBoundsExpression[] = [
     [
       [0, 0],
       [2500, 5200],
     ],
-    innerBounds,
-    innerBounds1,
-    innerBounds2,
-    outerBounds,
-    [
-      [0, 0],
-      [2500, 5200],
-    ],
   ];
 
-  const [bounds, setBounds] = useState<LatLngBoundsExpression | null>(
-    allBounds[0]
-  );
+  const [bounds, setBounds] = useState<LatLngBoundsExpression | null>(allBounds[0]);
 
   useEffect(() => {
-    console.log(bounds, "main" ,allBounds.indexOf(bounds as LatLngBoundsExpression));
-  }, [bounds ,allBounds]);
+    console.log(bounds, "main", allBounds.indexOf(bounds as LatLngBoundsExpression));
+  }, [bounds, allBounds]);
 
   return (
     <>
@@ -89,9 +46,9 @@ function App() {
               [0, 0],
               [height, width],
             ]}
-            dragging={false}
-            zoomControl={false}
-            style={{ height: "100vh", position: "fixed", top: 0, left: 0 }}
+            dragging={true}
+            zoomControl={true}
+            style={{ minHeight: "100vh", position: "static" }}
             scrollWheelZoom={false}
             minZoom={-1.8}
             crs={CRS.Simple}
@@ -104,40 +61,8 @@ function App() {
                 [height, width],
               ]}
             />
-
-            {bounds && <Point bounds={bounds as LatLngBoundsExpression} />}
           </MapContainer>
         )}
-
-        <div
-          ref={containerRef}
-          style={{
-            margin: 0,
-            padding: 0,
-            scrollSnapType: "y mandatory",
-            overflowY: "auto",
-            position: "sticky",
-            top: 0,
-            left: 0,
-            zIndex: 1,
-            display: "flex",
-            flexDirection: "column",
-            overflow: "hidden",
-            alignItems: "center",
-            justifyContent: "space-evenly",
-            flex: 1,
-          }}
-        >
-          {someText.map((item: string, idx: number) => (
-            <PointToObserve
-              containerRef={containerRef.current as HTMLDivElement}
-              idx={idx}
-              setBounds={(idx) => setBounds(allBounds[idx])}
-              key={idx}
-              item={item}
-            />
-          ))}
-        </div>
       </Suspense>
     </>
   );
